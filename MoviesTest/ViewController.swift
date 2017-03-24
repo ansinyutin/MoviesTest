9a4aa2fb2788ead3e2d7132f9a10de95fb825132
Синютин Андрей <andrew@macbook-pro-sinutin.local>
2017-03-24 22:07:34 +0300

    Initial Commit

---
+++ MoviesTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MoviesTest
+//
+//  Created by Синютин Андрей on 24.03.17.
+//  Copyright © 2017 Синютин Андрей. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
